9e9a1bce0ae95c9ed2c98f94c0d63ea65b35241f
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-07 21:07:14 +0200

    Import calculate_neighbours function in test

--- GameOfLife.py
+++ GameOfLife.py
@@ -2,7 +2,6 @@ board_size = 5
 
 def calculate_neighbours(board, current_box_row, current_box_column):
     neighbours = 0
-    # Calculate neighbours here!
     for row_index in range(current_box_row - 1, current_box_row + 2):
         for column_index in range(current_box_column - 1, current_box_column + 2):
             if row_index == current_box_row and column_index == current_box_column:
@@ -27,6 +26,7 @@ def print_board(board):
             print(column, " ", end="")
         print("")
 
-board = create_board(board_size)
-print_board(board)
+if __name__ == "__main__":
+    board = create_board(board_size)
+    print_board(board)
        
--- test.py
+++ test.py
@@ -1,16 +1,5 @@
 import unittest
-
-def calculate_neighbours(board, current_box_row, current_box_column):
-    neighbours = 0
-    # Calculate neighbours here!
-    for row_index in range(current_box_row - 1, current_box_row + 2):
-        for column_index in range(current_box_column - 1, current_box_column + 2):
-            if row_index == current_box_row and column_index == current_box_column:
-                continue
-            
-            if board[row_index][column_index] == '0':
-                neighbours += 1
-    return neighbours
+from GameOfLife import calculate_neighbours
 
 class GameOfLifeTest(unittest.TestCase):
 
